df81bd5e83072b2fa7d8163e3b8111392f0122c4
Saeed <52499523+saeed1986@users.noreply.github.com>
2021-04-23 09:05:19 +0200

    Arrays

--- variables.php
+++ variables.php
@@ -12,8 +12,22 @@ $string2 = 'World!';
 $greeting = $string1 .' '. $string2;
 echo $greeting;
 /////////////////////////////////////////
-
-*/
 define('greeting', 'Hello Everyone');
 echo greeting;
+/////////////////////////////////////////
+
+/////////////////////////////////////////
+*/
+#Array - Variable that holds multiple Values
+/*
+    -Indexed
+    -Associative
+    -Multi-dimensional
+*/
+
+//Indexed
+$people = array ('Kevin', 'Jeremy', 'Sara');
+$ids = array (23, 55, 12);
+$cars = ['Honday', 'Toyota', 'Ford'];
+//echo $people[0];
 ?>
