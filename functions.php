046e88c54af7b718ca32b5dc77deb23cc8dc7642
Saeed <52499523+saeed1986@users.noreply.github.com>
2021-04-23 18:57:12 +0200

    conditionals part added.

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+    # fUNCTIONS - BLOCK OF CODE THAT CAN BE REPEATEDLY CALLED
+    /*
+        Camel Case - myFunction()
+        Lower Case - my_function()
+        Pascal Case - MyFunction()
+    
+
+    function simpleFunction(){
+        echo 'Hello World';
+    }
+    simpleFunction();
+    
+    ////////////////////////////////7
+    
+    function sayHello($name){
+        echo "Hello $name<br>";
+    }
+    sayHello('Saeed');
+    sayHello('Behdis');
+    
+
+    ///////////////////////////////////
+    function addNumbers($num1,$num2){
+        echo $num1 + $num2;
+    }
+    addNumbers(2,3);
+    */
+    /////////////////////////////////
+    
+    // By Reference
+
+    
+?>
--- variables.php
+++ variables.php
@@ -16,18 +16,43 @@ define('greeting', 'Hello Everyone');
 echo greeting;
 /////////////////////////////////////////
 
-/////////////////////////////////////////
+
 */
 #Array - Variable that holds multiple Values
 /*
     -Indexed
     -Associative
     -Multi-dimensional
-*/
-
 //Indexed
 $people = array ('Kevin', 'Jeremy', 'Sara');
 $ids = array (23, 55, 12);
 $cars = ['Honday', 'Toyota', 'Ford'];
 //echo $people[0];
+/////////////////////////////////////////
+
+# LOOPS -Execute code set number of times
+
+/*
+    For
+    while
+    Do .. while
+    Foreach
+
+# For Loop
+# @params - init, condition, inc
+
+for($i = 0;$i < 10; $i++){
+    echo $i;
+    echo '<br>';
+}
+*/
+
+#Foreach Loop - For arrays
+$people = array ('Brad' => 'brad@gmail.com','Jose' => 'Jose@gmail.com' ,'William' => 'William@gmail.com');
+foreach($people as $person => $email){
+    echo $person. ': '. $email;
+    echo '<hr>';
+}
+
+
 ?>
