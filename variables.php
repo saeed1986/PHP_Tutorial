dbd420be3cde21855a6d1d7d988fa32f11192b0e
Saeed <52499523+saeed1986@users.noreply.github.com>
2021-04-23 08:10:00 +0200

    strings, numbers and ...

--- variables.php
+++ variables.php
@@ -1,1 +1,19 @@
-<h1>Hello</h1>
+<?php
+/*
+$output = 'Hello World!';
+echo $output;
+/////////////////////////////////////////
+$num1 = 4;
+$num2 = 10;
+$sum = $num1 * $num2;
+/////////////////////////////////////////
+$string1 = 'Hello';
+$string2 = 'World!';
+$greeting = $string1 .' '. $string2;
+echo $greeting;
+/////////////////////////////////////////
+
+*/
+define('greeting', 'Hello Everyone');
+echo greeting;
+?>
